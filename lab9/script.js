23b5a259103d9566b5cffe833a89c436e008fb57
yuann0214 <casper94214@gmail.com>
2025-04-09 14:07:41 +0800

    Add files via upload

---
+++ lab9/script.js
@@ -0,0 +1,20 @@
+var openUrl = "https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6";
+var xhr = new XMLHttpRequest();
+xhr.open('GET', openUrl, true);
+xhr.send();
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        dataset = JSON.parse(this.responseText);
+        addNewData(dataset);
+    }
+};
+function addNewData(dataset) {
+    var myTable = document.getElementById("csie");
+    dataset.forEach(function (data, index) {
+        var row = myTable.insertRow(-1); // 插入一個新列
+        row.insertCell(0).innerHTML = data['title']; // 名稱
+        row.insertCell(1).innerHTML = data['showInfo'][0]['location']; // 地點
+        row.insertCell(2).innerHTML = data['showInfo'][0]['price']; // 票價
+    });
+}
+
